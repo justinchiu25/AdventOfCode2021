e792ece50cb47c27bad7b73f3576b47bb8538262
Justin <justinchiu35@gmail.com>
2021-12-07 17:51:16 -0500

    Day 7

--- AdventofCode2021/Program.cs
+++ AdventofCode2021/Program.cs
@@ -6,7 +6,7 @@ namespace AdventofCode2021
     {
         static void Main(string[] args)
         {
-            var day = new Day6();
+            var day = new Day7();
 
             day.Main();
         }
---
+++ AdventofCode2021/Programs/Day7.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventofCode2021
+{
+    class Day7
+    {
+
+        public void Main()
+        {
+            string path = @"C:\Users\justi\Documents\GitHub\AdventOfCode2021\AdventofCode2021\Inputs\day7.txt";
+
+            string[] input = File.ReadAllLines(path);
+
+            int[] numbers = input[0].Split(",").Select(x => int.Parse(x)).ToArray();
+
+            Array.Sort(numbers);
+
+            PartOne(numbers);
+            PartTwo(numbers);
+            
+        }
+
+        private void PartOne(int[] input)
+        {
+            int leastCost = int.MaxValue;
+            int intResult = 0;
+            for (int i = 0; i < input.Length;i++)
+            {
+                int count = 0;
+                for (int k = 0; k < input.Length; k++)
+                {
+                    count += Math.Abs(input[i] - input[k]);
+                }
+
+                if (count < leastCost)
+                {
+                    leastCost = count;
+                    intResult = input[i];
+                }
+            }
+
+            Console.WriteLine(intResult+": " + leastCost);
+        }
+
+        private void PartTwo(int[] input)
+        {
+            int leastCost = int.MaxValue;
+            int intResult = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                int count = 0;
+                for (int k = 0; k < input.Length;k++)
+                {
+                    int difference = Math.Abs(input[i] - input[k]);
+                    count += (difference * (difference + 1)) / 2;
+                }
+
+                if (count < leastCost)
+                {
+                    leastCost = count;
+                    intResult = input[i];
+                }
+            }
+
+            Console.WriteLine(intResult + ": " + leastCost);
+        }
+
+        
+    }
+}
